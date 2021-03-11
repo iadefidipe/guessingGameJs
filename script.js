93143c50d843fb5f3b83fe9a7ab2756b6ea7a253
Israel Adefidipe <israeladefidipe@gmail.com>
2021-03-11 18:15:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**  console.log(document.querySelector('.message').textContent)
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13
+
+document.querySelector('.score').textContent = 4;
+
+document.querySelector('.guess').value = 46; **/
+
+const randomInteger = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+document.querySelector('.check').addEventListener('click', function (){
+    const guess= Number(document.querySelector('.guess').value);
+    console.log(document.querySelector('.guess').value);
+    const answer = randomInteger(5, 10);
+
+    if (answer === guess){
+        console.log( document.querySelector('.message').textContent = 'Correct guess' );
+        console.log(
+        (document.querySelector('.again').textContent = 'Correct!')
+        );
+        console.log(document.querySelector('.number').textContent = answer);
+    }
+    else if(!guess){
+        console.log( document.querySelector('.message').textContent = 'You have not inputed a guess')
+    }
+    else if (guess > 10 || guess < 5){
+        document.querySelector('.message').textContent = 'Try a number between 5 - 20';
+    }
+    else{
+        document.querySelector('.message').textContent = 'Keep guessing';
+        console.log(document.querySelector('.number').textContent = answer);
+    }
+} )
+
+
+
+
